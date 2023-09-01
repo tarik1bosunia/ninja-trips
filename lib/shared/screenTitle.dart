d3ba374b5ef5acdf09de1c8ad95cdbca7e489c5b
md tarik bosunia <bosuniamdtarik005@gmail.com>
2023-09-01 21:46:17 +0600

    tween_animaton

--- lib/shared/screenTitle.dart
+++ lib/shared/screenTitle.dart
@@ -3,14 +3,27 @@ import 'package:flutter/material.dart';
 class ScreenTitle extends StatelessWidget {
   final String text;
 
-
   const ScreenTitle({super.key, required this.text});
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: const TextStyle(fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+    return TweenAnimationBuilder(
+      tween: Tween<double>(begin: 0, end: 1),
+      duration: const Duration(microseconds: 500),
+      builder: (BuildContext context, double val, Widget? child) {
+        return Opacity(
+          opacity: val,
+          child: Padding(
+            padding: EdgeInsets.only(top: val * 20),
+            child: child,
+          ),
+        );
+      },
+      child: Text(
+        text,
+        style: const TextStyle(
+            fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+      ),
     );
   }
 }
